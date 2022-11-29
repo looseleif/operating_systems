667f4618952adfd36e2ff19b38f48c8cdf668471
SuaveSlav <esahbegovic@gmail.com>
2022-11-28 23:06:36 -0600

    FIFO progress

--- PA3/project3-release/main.cpp
+++ PA3/project3-release/main.cpp
@@ -13,6 +13,8 @@ how to use the page table and disk interfaces.
 #include <cassert>
 #include <iostream>
 #include <string.h>
+#include <queue>
+#include <map>
 
 using namespace std;
 
@@ -31,6 +33,10 @@ int *frameTable;
 // Pointer to disk for access from handlers
 struct disk *disk = nullptr;
 
+queue<int> free_frames;    //queue of free frames
+map<int, int> frame_table; //will give page number of frame
+//FIFO
+queue<int> q; //queue to keep track of entries order of insertion
 
 // Simple handler for pages == frames
 void page_fault_handler_example(struct page_table *pt, int page) {
@@ -60,13 +66,75 @@ void page_fault_handler_fifo(struct page_table *pt, int page) {
 	page_table_print(pt);
 	cout << "----------------------------------" << endl;
 
-	//page_table_get_entry(pt, page, ,)
+	// Map the page to the same frame number and set to read/write
+	int* retFrame = new int;
+	int* retBits = new int;
+	page_table_get_entry(pt, page, retFrame, retBits);
 
+	//page does not have a frame
+	if(*retBits == PROT_NONE)
+	{
+		//assign a frame and read in from disk
+		//we need a victim
+		if(free_frames.empty())
+		{
+			//find a victim and replace it
+			int victim_frame = q.front();
+			q.pop();
+
+			int victimPage = frame_table[victim_frame];
+			int* victimRetFrame = new int;
+			int* victimBits = new int;
+			page_table_get_entry(pt, victimPage, victimRetFrame, victimBits);
+
+			assert(victim_frame == *victimRetFrame);
+
+			//check dirty bit and write to disk if required
+			if(*victimBits & PROT_WRITE)
+			{
+				disk_write(disk, victimPage, page_table_get_physmem(pt) + victim_frame * PAGE_SIZE);
+			}
+			page_table_set_entry(pt, victimPage, 0, 0);
+
+			disk_read(disk, page, page_table_get_physmem(pt) + victim_frame * PAGE_SIZE);
+			page_table_set_entry(pt, page, victim_frame, PROT_READ);
+
+			frame_table[victim_frame] = page;
+			q.push(victim_frame);
+
+			delete victimRetFrame;
+			delete victimBits;
+		}
+		//assign the page to the free frame
+		else
+		{
+			int free_frame = free_frames.front();
+			free_frames.pop();
+
+			q.push(free_frame);
+			frame_table[free_frame] = page;
+
+			page_table_set_entry(pt, page, free_frame, PROT_READ);
+			disk_read(disk, page, page_table_get_physmem(pt) + (free_frame * PAGE_SIZE));
+		}
+	}
+	else if(*retBits & PROT_READ)
+	{
+		page_table_set_entry(pt, page, *retFrame, PROT_READ|PROT_WRITE);
+	}
+	else
+	{
+		cerr << "ERROR: page had unexpected permission bits values" << endl;
+		exit(1);
+	}
 
 	// Print the page table contents
 	cout << "After ----------------------------" << endl;
 	page_table_print(pt);
 	cout << "----------------------------------" << endl;
+
+	delete retFrame;
+	delete retBits;
 }
 
 // TODO - Handler(s) and page eviction algorithms
@@ -116,6 +184,7 @@ int main(int argc, char *argv[]) {
 	}
 
 	// TODO - Any init needed
+	struct page_table *pt = nullptr;
 
 	// Create a virtual disk
 	disk = disk_open("myvirtualdisk", npages);
@@ -124,18 +193,21 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 
-	if(!strcmp(algorithm, "fifo")){
+	if(!strcmp(algorithm, "fifo"))
+	{
 
-		struct page_table *pt = page_table_create(npages, nframes, page_fault_handler_fifo);
+		pt = page_table_create(npages, nframes, page_fault_handler_fifo);
 		if (!pt) {
 			cerr << "ERROR: Couldn't create page table: " << strerror(errno) << endl;
 			return 1;
 		}
 
-	} else {
+	} 
+	else 
+	{
 
 		// Create a page table
-		struct page_table *pt = page_table_create(npages, nframes, page_fault_handler_example /* TODO - Replace with your handler(s)*/);
+		pt = page_table_create(npages, nframes, page_fault_handler_example /* TODO - Replace with your handler(s)*/);
 		if (!pt) {
 			cerr << "ERROR: Couldn't create page table: " << strerror(errno) << endl;
 			return 1;
